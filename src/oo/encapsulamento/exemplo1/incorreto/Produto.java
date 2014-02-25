371933700d8fcf5d1f88ed6711dfe025cb04cf2a
Andre Willik Valenti <awvalenti@gmail.com>
2014-02-25 17:53:01 -0300

    Correções

---
+++ src/oo/encapsulamento/exemplo1/incorreto/Produto.java
@@ -0,0 +1,13 @@
+package oo.encapsulamento.exemplo1.incorreto;
+
+import java.math.BigDecimal;
+
+public class Produto {
+
+	private BigDecimal valor;
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+}
