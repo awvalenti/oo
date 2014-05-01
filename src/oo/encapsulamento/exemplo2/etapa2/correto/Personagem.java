c71dc722cb651559ebd4daadcf7c2a6f89be3895
André Willik Valenti <awvalenti@gmail.com>
2014-05-01 14:35:17 -0300

    Removendo comentários

--- src/oo/encapsulamento/exemplo2/etapa2/correto/Personagem.java
+++ src/oo/encapsulamento/exemplo2/etapa2/correto/Personagem.java
@@ -22,24 +22,17 @@ public class Personagem {
 		int dano = 0;
 
 		if (invencivel) {
-			// Nenhum dano, por estar invencivel
 			dano = 0;
 		} else if (inimigo.ehMenor()) {
-			// Dano de inimigo menor: 1
 			dano = 1;
 		} else if (inimigo.ehMaior()) {
-			// Dano de inimigo maior: 2
 			dano = 2;
 		} else if (inimigo.ehChefe()) {
-			// Dano de chefe: todos os pontos de vida
 			dano = pontosDeVida;
 		}
 
 		if (dano > 0) {
-			// Aplica dano apropriado
 			pontosDeVida -= dano;
-
-			// Quando sofre dano, personagem perde os caquis coletados
 			contagemDeCaquis = 0;
 		}
 	}
